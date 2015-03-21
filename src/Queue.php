31c2e3de9e24e7578c08dc5184a36e32c16e2923
Juriy Panasevich <elfelrandcor@gmail.com>
2015-03-21 13:43:50 +0300

    Абстракция для очереди, заглушка для сохранения задания

--- src/Queue.php
+++ src/Queue.php
@@ -5,7 +5,22 @@
 
 namespace JuriyPanasevich\BJobs;
 
+use JuriyPanasevich\BJobs\Interfaces\IJob;
+use JuriyPanasevich\BJobs\Interfaces\IQueue;
 
-abstract class Queue {
+abstract class Queue implements IQueue {
 
+    public function pushOn($name, $job, $data = null) {
+        return $this->push($job, $data, $name);
+    }
+
+    /**
+     * @param IJob $job
+     * @param mixed $data
+     * @param string $name
+     * @return boolean
+     */
+    public function push($job, $data = null, $name = null) {
+        return $job->save();
+    }
 }
