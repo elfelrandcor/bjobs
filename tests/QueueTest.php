00f0d7e095c3fb8a425ad0d7fcd82c9f3e1cba1d
Juriy Panasevich <elfelrandcor@gmail.com>
2015-11-17 16:39:48 +0300

    Тестирование

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -1,7 +1,10 @@
 <?php
 use JuriyPanasevich\BJobs\Interfaces\JobInterface;
 use JuriyPanasevich\BJobs\Job;
+use JuriyPanasevich\BJobs\Journal\QueueJournal;
 use JuriyPanasevich\BJobs\Queue;
+use JuriyPanasevich\BJobs\QueueWorker;
+use JuriyPanasevich\Logger\Logger;
 
 /**
  * @author Juriy Panasevich <juriy.panasevich@gmail.com>
@@ -12,6 +15,16 @@ class QueueTest extends PHPUnit_Framework_TestCase {
     public function testPush() {
         $queue = new QueueTest__Queue();
         $this->assertEquals(true, $queue->pushOn('test', new QueueTest__Job()));
+
+        $queue = new QueueTest__Queue('test');
+
+        $journal = new QueueJournal();
+        $journal->setEntity($queue);
+
+        $logger = new Logger($journal);
+
+//        $worker = new QueueWorker($queue, $logger);
+//        $worker->run();
 
         $this->markTestIncomplete();
     }
@@ -33,11 +46,27 @@ class QueueTest__Queue extends Queue {
     public function remove(JobInterface $job) {
         return true;
     }
+
+    /**
+     * @param JobInterface $job
+     * @return boolean
+     */
+    public function push(JobInterface $job) {
+        return true;
+    }
 }
 
 class QueueTest__Job extends Job {
 
     public function __invoke() {
         return true;
+    }
+
+    public function getDateRelease() {
+        return new DateTime();
+    }
+
+    public function setDateRelease($date) {
+        return $this;
     }
 }
