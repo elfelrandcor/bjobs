8f8c182993ba2a32d020224b111af864de59db2a
Juriy Panasevich <elfelrandcor@gmail.com>
2015-11-17 16:29:58 +0300

    Журнал

---
+++ src/Journal/QueueJournalLog.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Juriy Panasevich <panasevich@worksolutions.ru>
+ */
+
+namespace JuriyPanasevich\BJobs\Journal;
+
+
+use JuriyPanasevich\Logger\Journal\AbstractJournal;
+use JuriyPanasevich\Logger\Journal\AbstractJournalLog;
+
+class QueueJournalLog extends AbstractJournal {
+
+    public function setEntity($entity) {
+
+    }
+
+    public function getEntity() {
+        // TODO: Implement getEntity() method.
+    }
+
+    /** @return AbstractJournalLog */
+    public function getJournalLog() {
+        // TODO: Implement getJournalLog() method.
+    }
+
+    public function find() {
+        // TODO: Implement find() method.
+    }
+
+    public function save() {
+        // TODO: Implement save() method.
+    }
+}
