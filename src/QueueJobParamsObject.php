ba22c3e86ed51ccf93041ae2879ba569c4509527
Juriy Panasevich <elfelrandcor@gmail.com>
2016-05-14 14:13:19 +0300

    Класс для хранения параметров задач

---
+++ src/QueueJobParamsObject.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Juriy Panasevich <juriy.panasevich@gmail.com>
+ */
+
+namespace JuriyPanasevich\BJobs;
+
+
+class QueueJobParamsObject {
+
+    protected $_params = [];
+
+    public function getParams() : array {
+        return $this->_params;
+    }
+
+    public function setParams(array $params) {
+        $this->_params = $params;
+        return $this;
+    }
+
+    public function addParam(string $name, $value = null) {
+        $this->_params[$name] = $value;
+        return $this;
+    }
+
+    public function getParam($name) {
+        return $this->_params[$name];
+    }
+}
