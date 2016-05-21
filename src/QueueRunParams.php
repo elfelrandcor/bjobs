f94e641519c4aa4722a5d444d83ec04e6585f32e
Juriy Panasevich <elfelrandcor@gmail.com>
2016-05-21 12:28:27 +0300

    Параметры для запуска очереди

---
+++ src/QueueRunParams.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Juriy Panasevich <juriy.panasevich@gmail.com>
+ */
+
+namespace JuriyPanasevich\BJobs;
+
+
+/**
+ * Class QueueRunParams
+ * @package JuriyPanasevich\BJobs
+ * @method getDelay
+ * @method setDelay($seconds)
+ * @method setMemory($mbytes)
+ * @method getMemory()
+ * @method setSleep($seconds)
+ * @method getSleep()
+ * @method setMaxTries($tries)
+ * @method getMaxTries()
+ * @method setStopAfter($count)
+ * @method getStopAfter()
+ */
+class QueueRunParams extends ParamsObject {
+
+}
